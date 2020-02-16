3bd4cf84e03fd4676cd2fc49ef4f880951cf97fd
Adam-J-G <36233205+Adam-J-G@users.noreply.github.com>
2020-02-16 14:26:51 +0000

    Add files via upload

--- render.cpp
+++ render.cpp
@@ -9,15 +9,15 @@ void render(BelaContext *context, void *userData)
 {
 	for(unsigned int frame = 0; frame < context->audioFrames; frame++) {
 		float out_l = 0;
-        float out_r = 0;
-         
-        // Read audio inputs
-        out_l = audioRead(context, frame, 0);
-        out_r = audioRead(context, frame, 1);
-        
+		float out_r = 0;
+		
+		// Read audio inputs
+		out_l = audioRead(context, frame, 0);
+		out_r = audioRead(context, frame, 1);
+		
 		// Write the sample into the output buffer
-        audioWrite(context, frame, 0, out_l);
-        audioWrite(context, frame, 1, out_r);
+		audioWrite(context, frame, 0, out_l);
+		audioWrite(context, frame, 1, out_r);
 	}
 }
 
