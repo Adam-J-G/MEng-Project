f39c4ef972390964ad292ab02ca030fb61daba2c
Adam-J-G <36233205+Adam-J-G@users.noreply.github.com>
2020-02-16 14:22:38 +0000

    Add files via upload

---
+++ render.cpp
@@ -0,0 +1,27 @@
+#include <Bela.h>
+
+bool setup(BelaContext *context, void *userData)
+{
+	return true;
+}
+
+void render(BelaContext *context, void *userData)
+{
+	for(unsigned int frame = 0; frame < context->audioFrames; frame++) {
+		float out_l = 0;
+        float out_r = 0;
+         
+        // Read audio inputs
+        out_l = audioRead(context, frame, 0);
+        out_r = audioRead(context, frame, 1);
+        
+		// Write the sample into the output buffer
+        audioWrite(context, frame, 0, out_l);
+        audioWrite(context, frame, 1, out_r);
+	}
+}
+
+void cleanup(BelaContext *context, void *userData)
+{
+
+}
