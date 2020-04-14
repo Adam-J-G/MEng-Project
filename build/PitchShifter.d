e7faa520f3ca4c715023db84744ed5413e768080
Adam-J-G <ag1333@york.ac.uk>
2020-04-14 17:16:50 +0000

    Remove files

--- build/PitchShifter.d
+++
@@ -1,5 +0,0 @@
-/root/Bela/projects/MEng-Project/build/PitchShifter.o: \
-  /root/Bela/projects/MEng-Project/PitchShifter.cpp \
-  /root/Bela/projects/MEng-Project/PitchShifter.h
-
-/root/Bela/projects/MEng-Project/PitchShifter.h:
--- build/Reverberator.d
+++
@@ -1,5 +0,0 @@
-/root/Bela/projects/MEng-Project/build/Reverberator.o: \
-  /root/Bela/projects/MEng-Project/Reverberator.cpp \
-  /root/Bela/projects/MEng-Project/Reverberator.h
-
-/root/Bela/projects/MEng-Project/Reverberator.h:
